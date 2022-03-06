7d6f7abbc9fbbc5ef08940c3b17478d1ea130c17
michellebazan <24michelle.b@gmail.com>
2022-03-06 11:36:38 -0800

    thanos won

--- src/_01_Sorting_Algorithms/ThanosSorter.java
+++ src/_01_Sorting_Algorithms/ThanosSorter.java
@@ -18,20 +18,23 @@ public class ThanosSorter extends Sorter {
 	 * that isn't greater than the previous one is 97 (12, 100, 200, 201 are all in
 	 * order). In quiet rage, Thanos randomly picks half of the elements in the
 	 * array and removes them. In this case he removes the second half, so the array
-	 * now looks like: {12, 100, 200, 201, 0, 0, 0, 0} // second half removed
+	 * now looks like: 
+	 * {12, 100, 200, 201, 0, 0, 0, 0} // second half removed
 	 * 
 	 * The remaining non-0 values are now in order and balance is restored to the
-	 * array. If Thanos removed the first half, the array would be: {0, 0, 0, 0, 97,
-	 * 33, 100, 687}
+	 * array. If Thanos removed the first half, the array would be: 
+	 * {0, 0, 0, 0, 97, 33, 100, 687}
 	 * 
 	 * The first unordered number is 33, so Thanos snaps his fingers and removes
-	 * half again: {0, 0, 0, 0, 0, 0, 100, 687} // first half or {0, 0, 0, 0, 97,
-	 * 33, 0, 0} // second half
+	 * half again: 
+	 * {0, 0, 0, 0, 0, 0, 100, 687} // first half or 
+	 * {0, 0, 0, 0, 97, 33, 0, 0} // second half
 	 * 
 	 * If the first half is removed the result is sorted. If the second half is
 	 * removed 33 isn't sorted so Thanos snaps his fingers again! The result is
-	 * either: {0, 0, 0, 0, 0, 33, 0, 0} // first half or {0, 0, 0, 0, 97, 0, 0, 0}
-	 * // second half
+	 * either: 
+	 * {0, 0, 0, 0, 0, 33, 0, 0} // first half or 
+	 * {0, 0, 0, 0, 97, 0, 0, 0} // second half
 	 * 
 	 * NOTE: The algorithm doesn't necessarily have to take away half from the front
 	 * or back of the array. You can implement an algorithm that takes elements away
@@ -45,35 +48,49 @@ public class ThanosSorter extends Sorter {
 		int min = 0;
 		boolean isOrdered = false;
 		int orange = 0;
+		int secondhalf = max / 2;
+		boolean isFirstRound = true;
 
 		while (isOrdered == false) {
-			for (int i = min; i < max - 1; i++) {
+			for (int i = 0; i < arr.length - 1; i++) {
+				display.updateDisplay();
+
 				if (arr[i] > arr[i + 1]) {
+					display.updateDisplay();
+
 					orange++;
 				}
 			}
 
 			if (orange >= 0) {
-				//System.out.println("I am.. Inevitable");
+				System.out.println("I am.. Inevitable");
 				orange = 0;
 
 				Random rand = new Random();
 				int randy = rand.nextInt(2);
-//i need to fix min and max lol... it is infinite loop
 				if (randy == 1) {
-					//second half
-					min = (max) / 2;
-					max = arr.length;
+				// second half
+					min = (max-min) / 2;
+					// max = arr.length;
 					for (int k = min; k < max; k++) {
 						arr[k] = 0;
 					}
+					display.updateDisplay();
+					
+					//???
+					max = min;
+					
 				} else {
-					//first half
-					max = (max) / 2;
-					min = 0;
-					for (int q = min; q < max; q++) {
+					// first half
+					
+					int tempMax = (max) / 2;
+
+					for (int q = min; q < tempMax; q++) {
 						arr[q] = 0;
 					}
+					
+					//???
+					min = max/2;
 				}
 				display.updateDisplay();
 			} else {
